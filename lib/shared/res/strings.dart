5f7d54bf0ff5e13a1b99aa41763aad0e133fd416
Raks-Javac <rufaikudus2014@gmail.com>
2023-04-01 22:34:26 +0100

    added fonts

--- lib/shared/res/strings.dart
+++ lib/shared/res/strings.dart
@@ -1,9 +1,9 @@
 part of "res.dart";
 
 abstract class PStrings {
-  static String appName = 'DictionaryApp';
+  static String appName = 'Know More';
 
-  static const String boldFontName = "PBold";
-  static const String mediumFontName = "PMedium";
-  static const String lightFontName = "PLight";
+  static const String boldFontName = "RBold";
+  static const String mediumFontName = "RMedium";
+  static const String lightFontName = "RLight";
 }
