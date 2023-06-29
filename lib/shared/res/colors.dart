cf52f3c0350c07258cc193de79e2beae59874b5f
Raks-Javac <rufaikudus2014@gmail.com>
2023-06-29 23:17:47 +0100

    ref:

--- lib/shared/res/colors.dart
+++ lib/shared/res/colors.dart
@@ -3,9 +3,9 @@ part of "res.dart";
 abstract class DColors {
   DColors._();
 
-  static const Color primaryColor = Color(0xFF8AAEFD);
+  static const Color primaryColor = Color(0xFF1DB954);
   static const Color secondaryColor = Color(0xFFE2D55B);
-  static const Color darkBAckgroundColor = Color(0xFF161616);
+  static const Color darkBAckgroundColor = Color(0xFF111111);
   static const Color white = Colors.white;
   static const Color redAccent = Colors.redAccent;
 }
