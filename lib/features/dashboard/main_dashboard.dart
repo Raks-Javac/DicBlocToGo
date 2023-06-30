56d5c058b0941d5a9ab0e53a6f16db707eafa064
Raks-Javac <rufaikudus2014@gmail.com>
2023-06-30 09:05:45 +0100

    ref:

--- lib/features/dashboard/main_dashboard.dart
+++ lib/features/dashboard/main_dashboard.dart
@@ -1,9 +1,11 @@
 import 'package:dict_app/core/utils/extensions.dart';
 import 'package:dict_app/features/book_mark/views/bookmark_view.dart';
+import 'package:dict_app/features/home/blocs/home_bloc.dart';
 import 'package:dict_app/features/home/views/home_screen.dart';
 import 'package:dict_app/features/settings/views/settings_view.dart';
 import 'package:dict_app/shared/res/res.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MainDashboardScreen extends StatefulWidget {
@@ -18,29 +20,29 @@ class _MainDashBoardScreenState extends State<MainDashboardScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
-  final List<Tab> _tabs = [
-    const Tab(
-      iconMargin: EdgeInsets.only(top: 10),
-      icon: ImageIcon(
-        AssetImage(nAHomeIcon),
-      ),
-      text: '',
-    ),
-    const Tab(
-      iconMargin: EdgeInsets.only(top: 10),
-      icon: ImageIcon(
-        AssetImage(nABookMarkIcon),
-      ),
-      text: '',
-    ),
-    const Tab(
-      iconMargin: EdgeInsets.only(top: 10),
-      icon: ImageIcon(
-        AssetImage(nASettingsIcon),
-      ),
-      text: '',
-    ),
-  ];
+  List<Tab> tabs({ThemeBloc? themeBloc}) => [
+        const Tab(
+          iconMargin: EdgeInsets.only(top: 10),
+          icon: ImageIcon(
+            AssetImage(nAHomeIcon),
+          ),
+          text: '',
+        ),
+        const Tab(
+          iconMargin: EdgeInsets.only(top: 10),
+          icon: ImageIcon(
+            AssetImage(nABookMarkIcon),
+          ),
+          text: '',
+        ),
+        const Tab(
+          iconMargin: EdgeInsets.only(top: 10),
+          icon: ImageIcon(
+            AssetImage(nASettingsIcon),
+          ),
+          text: '',
+        ),
+      ];
 
   final List<Widget> _views = [
     const DictionaryHomeView(),
@@ -51,7 +53,7 @@ class _MainDashBoardScreenState extends State<MainDashboardScreen>
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: _tabs.length, vsync: this);
+    _tabController = TabController(length: tabs().length, vsync: this);
   }
 
   @override
@@ -62,30 +64,38 @@ class _MainDashBoardScreenState extends State<MainDashboardScreen>
 
   @override
   Widget build(BuildContext context) {
+    final homeBloc = BlocProvider.of<HomeActivityBloc>(context);
     return Scaffold(
       body: TabBarView(
         physics: const NeverScrollableScrollPhysics(),
         controller: _tabController,
         children: _views,
       ),
-      bottomNavigationBar: SizedBox(
-        height: 65.h,
-        child: TabBar(
-          indicator: BoxDecoration(
-            border: Border(
-              top: BorderSide(
-                color: context.appTheme.primaryColor,
-                width: 3.0,
+      bottomNavigationBar: Builder(builder: (context) {
+        final themeBloc = context.watch<ThemeBloc>();
+        return SizedBox(
+          height: 65.h,
+          child: TabBar(
+            indicator: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: context.appTheme.primaryColor,
+                  width: 3.0,
+                ),
               ),
             ),
+            labelStyle: context.appTextTheme.bodySmall,
+            controller: _tabController,
+            tabs: tabs(themeBloc: themeBloc),
+            labelColor: themeBloc.isDarkMode == false
+                ? WColors.primaryColor
+                : WColors.white,
+            unselectedLabelColor: themeBloc.isDarkMode == false
+                ? WColors.barBlackColor
+                : WColors.white,
           ),
-          labelStyle: context.appTextTheme.bodySmall,
-          controller: _tabController,
-          tabs: _tabs,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.grey,
-        ),
-      ),
+        );
+      }),
     );
   }
 }
